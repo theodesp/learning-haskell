76316bb96b987cac3b0de9f07d89648a5efcda58
theodesp <theo.despoudis@wpengine.com>
2020-07-15 21:10:25 +0100

    Let expression

--- scratch.hs
+++ scratch.hs
@@ -17,4 +17,19 @@ calcChange owed given = if change > 0
                         then change
                         else 0
   where change = given - owed
-                                  
+
+ident = \x -> x
+
+sumSquareOrSquareSum x y = if sumSquare > squareSum
+                           then sumSquare
+                           else squareSum
+  where sumSquare = x^2 + y^2
+        squareSum = (x+y)^2
+
+overwrite x = let x = 2
+              in
+                let x = 3
+                in
+                  let x = 4
+                  in
+                    x
