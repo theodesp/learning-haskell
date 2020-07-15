f4f797f971c7e1aed6f2103c0f1ab3c19d0949d7
theodesp <theo.despoudis@wpengine.com>
2020-07-15 20:58:24 +0100

    Where statment

--- scratch.hs
+++ scratch.hs
@@ -7,10 +7,14 @@ toPart recipient = "Dear " ++ recipient ++ ",\n"
 bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
 fromPart author = "Thanks,\n"++author
 
+largeNumber = 2^123
+
 createEmail recipient title author = toPart recipient ++
                                      bodyPart title ++
                                      fromPart author
 
-
-let largeNumber = 2^123
+calcChange owed given = if change > 0
+                        then change
+                        else 0
+  where change = given - owed
                                   
